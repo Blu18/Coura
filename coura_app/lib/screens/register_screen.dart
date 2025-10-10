58ac39546b82cd5024aa5c7f88988064dfe912eb
Blu18 <diego.manzo@uabc.edu.mx>
2025-10-09 22:51:17 -0700

    Ya se pueden registrar usuarios

--- coura_app/lib/screens/register_screen.dart
+++ coura_app/lib/screens/register_screen.dart
@@ -1,8 +1,11 @@
+import 'package:coura_app/screens/home_screen.dart';
 import 'package:coura_app/screens/login_screen.dart';
 import 'package:coura_app/utils/custom/custom_text_field.dart';
 import 'package:coura_app/utils/styles/app_colors.dart';
 import 'package:coura_app/utils/styles/app_images.dart';
 import 'package:coura_app/utils/styles/text_style.dart';
+import 'package:coura_app/services/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -16,6 +19,64 @@ class _RegisterScreenState extends State<RegisterScreen> {
   TextEditingController nombrecontroller = TextEditingController();
   TextEditingController correocontroller = TextEditingController();
   TextEditingController contracontroller = TextEditingController();
+  // final _formKey = GlobalKey<FormState>();
+  String errorMessage = '';
+
+  @override
+  void dispose() {
+    nombrecontroller.dispose();
+    correocontroller.dispose();
+    contracontroller.dispose();
+    super.dispose();
+  }
+
+  void register() async {
+    try {
+      await authService.value.createAccount(
+        email: correocontroller.text,
+        password: contracontroller.text,
+      );
+      popPage();
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    } on FirebaseAuthException catch (e) {
+      String translatedError = "Ocurrio un error";
+      switch (e.code) {
+        case 'weak-password':
+          translatedError = "La contraseña debe tener al menos 6 caracteres.";
+          break;
+        case 'email-already-in-use':
+          translatedError =
+              "Este correo electrónico ya está en uso por otra cuenta.";
+          break;
+        case 'invalid-email':
+          translatedError = "Correo electrónico invalido.";
+          break;
+        case 'operation-not-allowed':
+          translatedError =
+              "La creación de cuentas con correo y contraseña no está habilitada.";
+          break;
+        case 'network-request-failed':
+          translatedError = "Error de red. Revisa tu conexión a internet.";
+          break;
+        default:
+          translatedError =
+              "No se pudo registrar el usuario. Por favor, intenta de nuevo.";
+          print(
+            'Error no manejado: ${e.code}',
+          ); // Imprime el código para depuración
+      }
+      setState(() {
+        errorMessage = translatedError;
+      });
+    }
+  }
+
+  void popPage() {
+    Navigator.pop(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +112,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 color: const Color.fromARGB(255, 80, 80, 80),
               ),
             ),
-        
+
             Padding(
               padding: const EdgeInsets.all(12.0),
               child: Column(
@@ -62,39 +123,47 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     title: "Nombre Completo",
                     controller: nombrecontroller,
                     hintText: "Ej: Juan Pérez",
+                    isThisPassword: false,
+                    isthisRequired: true,
                   ),
                   CustomField(
                     title: "Correo Electrónico",
                     controller: correocontroller,
                     hintText: "Ej: tu-correo@dominio.com",
+                    isThisPassword: false,
+                    isthisRequired: true,
                   ),
                   CustomField(
                     title: "Contraseña",
                     controller: contracontroller,
                     hintText: "Mínimo 6 caracteres",
                     isThisPassword: true,
+                    isthisRequired: true,
                   ),
-        
-                  SizedBox(height: 2),
-                  Container(
-                    height: 45,
-                    decoration: BoxDecoration(
-                      color: AppColors.lapizlazuli,
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "REGISTRARME",
-                        style: CTextStyle.titleMediumbold.copyWith(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
+                  Text(
+                    errorMessage,
+                    style: CTextStyle.bodyMedium.copyWith(color: Colors.red),
                   ),
                 ],
               ),
             ),
-        
+            SizedBox(height: 2),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.lapizlazuli,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+              onPressed: () {
+                register();
+              },
+              child: Text(
+                "REGISTRARME",
+                style: CTextStyle.titleMediumbold.copyWith(color: Colors.white),
+              ),
+            ),
+
             Padding(
               padding: const EdgeInsets.all(12.0),
               child: Column(
@@ -104,7 +173,10 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     spacing: 7.5,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("¿Ya tienes una cuenta?", style: CTextStyle.bodySmall),
+                      Text(
+                        "¿Ya tienes una cuenta?",
+                        style: CTextStyle.bodySmall,
+                      ),
                       TextButton(
                         onPressed: () {
                           Navigator.push(
@@ -123,7 +195,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       ),
                     ],
                   ),
-        
+
                   SizedBox(height: 10),
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
